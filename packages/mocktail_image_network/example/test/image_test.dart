1590b45ba488b4187bf4048ab65520fe6688d254
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2024-05-25 16:24:17 -0300

    replace image by SvgPicture

--- packages/mocktail_image_network/example/test/image_test.dart
+++ packages/mocktail_image_network/example/test/image_test.dart
@@ -1,11 +1,11 @@
 import 'package:example/main.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 
 void main() {
   testWidgets('can use mocktail for network images', (tester) async {
     await mockNetworkImages(() async => tester.pumpWidget(const FakeApp()));
-    expect(find.byType(Image), findsOneWidget);
+    expect(find.byType(SvgPicture), findsOneWidget);
   });
 }
